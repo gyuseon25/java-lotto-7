c8c000be828d90eda362d3d05b1776281e292c50
gyuseon <mn5592@naver.com>
2024-11-04 21:54:07 +0900

    Feat: 로또 번호 발행 기능 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+
+    public List<List<Integer>> getLottoNumbers(int count) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottoNumbers.add(generateLottoNumbers());
+        }
+        return lottoNumbers;
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
