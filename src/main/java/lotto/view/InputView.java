1018c82890da317c36332b115996b546c13e9285
gyuseon <mn5592@naver.com>
2024-11-04 22:54:47 +0900

    Refactor: InputView 싱글톤 패턴으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final InputView INSTANCE = new InputView();
+
+    public static InputView getInstance() {
+        return INSTANCE;
+    }
+
+    private InputView() {
+    }
+
     public static String readAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
